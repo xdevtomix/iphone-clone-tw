9b0b551ef7d3d6a896a23f3a26dffe81ea151aa9
tomi <dev.dreissiger@gmail.com>
2021-09-28 00:21:29 +0200

    notch created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { ChartBarIcon, WifiIcon, TruckIcon, LockOpenIcon, LightBulbIcon, CameraIcon } from '@heroicons/react/solid';
 
+import Notch from './components/Notch';
+
 export default function App() {
   return (
     <div className="grid min-h-screen bg-purple-50 place-items-center">
-      iphone
+      <div className="mx-auto h-[712px] w-[350px] bg-black rounded-[60px] border-[14px] border-black relative overflow-hidden ring ring-purple-400 shadow-xl">
+        <img className="absolute inset-0 h-full w-full object-cover" src="https://cdn.pixabay.com/photo/2021/06/28/18/36/cereals-6372410_960_720.jpg" />
+        <Notch />
+      </div>
     </div>
   )
 }
---
+++ src/components/Notch.jsx
@@ -0,0 +1,7 @@
+export default function Notch() {
+    return (
+        <div className="absolute top-0 inset-x-0">
+            <div className="mx-auto bg-black h-6 w-40 rounded-b-2xl"></div>
+        </div>
+    );
+}
