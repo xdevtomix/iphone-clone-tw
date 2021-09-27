273cb3807c7d2cdaf6529a1ae6dfdb7f0b4bd870
tomi <dev.dreissiger@gmail.com>
2021-09-28 01:49:13 +0200

    notification box in

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,12 @@
 import TimeAndDate from './TimeAndDate';
+import NotificationBox from './NotificationBox';
 
 export default function Main() {
     return (
         <div className="relative">
             <TimeAndDate />
 
-            
+            <NotificationBox />
         </div>
     );
 }
---
+++ src/components/NotificationBox.jsx
@@ -0,0 +1,27 @@
+export default function NotificationBox() {
+    return (
+        <div className="relative mt-4 mx-2">
+            <div className="p-4 bg-white/40 backdrop-blur-md rounded-3xl shadow">
+
+                <div className="flex justify-between items-start">
+                    <div>
+                        <p className="text-xs font-bold">9:30 AM</p>
+                        <h2 className="text-lg font-bold">Your Morning Summary</h2>
+                    </div>
+                    <span className="bg-gray-500 h-8 w-8 rounded-full text-white flex items-center justify-center">11</span>
+                </div>
+
+                <div>
+
+                </div>
+
+                <hr />
+
+                <div>
+                    
+                </div>
+
+            </div>
+        </div>
+    );
+}
